575bab5dc1551e55fa72c7daa92c1d358dc73eff
Hoangphohieu <hoangphohieu1@gmail.com>
2019-12-13 19:59:41 -0800

    hieu

--- MShopee.jsx
+++ MShopee.jsx
@@ -4,13 +4,38 @@ if (!folderSaveJPG.exists) {
       folderSaveJPG.create();
 }
 jpgOption = new JPEGSaveOptions();
-jpgOption.quality = 11;
+jpgOption.quality = 10;
 
 
 
 if (theFiles) {
+      app.documents.add(1200, 2400, 300, "aaaa");
       for (var m = 0; m < theFiles.length; m++) {
             var theNewNameSelect = theFiles[m].name.match(/(.*)\.[^\.]+$/)[1];
+
+            app.open(File(theFiles[m]), OpenDocumentType.JPEG, true);
+            var width = app.activeDocument.width;
+            var height = app.activeDocument.height;
+            if (height / width >= 2) {
+                  height = height * 1200 / width;
+                  width = 1200;
+                  app.activeDocument.resizeImage(1200, height * 1200 / width);
+            }
+            else {
+                  width = width * 2400 / height;
+                  height = 2400;
+                  app.activeDocument.resizeImage(width * 2400 / height, 2400);
+            }
+            app.activeDocument.activeLayer.duplicate(app.documents["aaaa"]);
+            app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+            app.activeDocument.activeLayer.translate((1200 - width) / 2, (2400 - height) / 2);
+            app.activeDocument.saveAs(Folder(theFiles[m]), jpgOption, true, Extension.LOWERCASE);
+            app.activeDocument.activeLayer.remove();
+
+
+
+
+
             var customerChildren = Folder("~/Desktop/mockup phoneCase").getFiles("*.tif");
             Folder("~/Desktop/file phoneCase/" + theNewNameSelect).create();
             for (var i = 0; i < customerChildren.length; i++) {
@@ -31,6 +56,8 @@ if (theFiles) {
             app.activeDocument.saveAs(Folder("~/Desktop/file phoneCase/" + theNewNameSelect + "/" + theNewNameSelect + ".jpg"), jpgOption, true, Extension.LOWERCASE);
             app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
       }
+      app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+
 }
 
 
